621ec5e8f998e499db7c0b5f577a51f22f2afc9e
Sean Dingemans <sxd616@cs.bham.ac.uk>
2018-07-16 18:03:06 +0100

    Setup start script and heroku port

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const hbs =require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;  //process.env is object that stores all of enviro variables as key-value pairs
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
@@ -76,6 +77,6 @@ app.get('/bad', (req, res) => {
   });// end res.send
 });// end get method and request fxn
 
-app.listen(3000, () => {
-  console.log('Server is up on port 3000');
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
 });// end listen method
